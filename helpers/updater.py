7674a798d2ec3e6b8ea1aa01476b7640410605fb
doomium-chloride <47913762+doomium-chloride@users.noreply.github.com>
2021-06-06 21:01:43 +1200

    Fix update bug

--- helpers/updater.py
+++ helpers/updater.py
@@ -4,21 +4,35 @@ import os
 
 azurapi_version = "./azurapi_data/version-info.json"
 
+
+async def clear_version(client, update_msg):
+    try:
+        os.remove(azurapi_version)
+        await message_me(client, update_msg)
+    except OSError as e:
+        print("Error: %s - %s." % (e.filename, e.strerror))
+        await message_me(client, "Version couldn't be cleared when updating...")
+
+
 async def update_azurapi(client, forced=False):
     api = AzurAPI()
+
+    # forced means no checking
+    if forced:
+        await clear_version(client, "cleared azurapi version")
+        api.updater.updateShipsData()
+        api.updater.updateEquipmentsData()
+        print("force updated")
+        await message_me(client, "force updated")
+        return
+
+    # not forced so check first
     print("Checking for AzurApi updates...")
     need_update = api.updater.checkForNewUpdate()
     if any(need_update):
         update_msg = "AzurApi updating from: {0}".format(api.getVersion())
         print("Updating", update_msg)
-        try:
-            os.remove(azurapi_version)
-            await message_me(client, update_msg)
-        except OSError as e:
-            print ("Error: %s - %s." % (e.filename, e.strerror))
-            await message_me(client, "Version couldn't be cleared when updating...")
+        await clear_version(client, update_msg)
         api.updater.update()
     else:
         print("No update required for Azur Api")
-        if forced:
-            await message_me(client, "No update required for Azur Api")
